4ae86573e6aba251dfaceb4f028cb22f0fba42d7
Adham Sameh <adham.s.salama@gmail.com>
2024-12-04 11:32:15 +0400

    Fixed a minor error in the master API router caused by a mistake in merging the server/api/__init__.py file

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -5,11 +5,6 @@ master_router = APIRouter()
 master_router.include_router(cart_router)
 master_router.include_router(wallet_router)
 master_router.include_router(user_router)
-
-from .routers import cart_router
-from .routers import order_router
-
-master_router = APIRouter()
-master_router.include_router(cart_router)
 master_router.include_router(order_router)
+
 
