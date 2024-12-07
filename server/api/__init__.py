4c31c955aa2449e3ccd7bc5006c07444b7655a3f
Saif Eldeen Eid <saifeyad2005@gmail.com>
2024-12-07 16:09:34 +0400

    Fix: Added items_router to main router for category endpoint
    version 2

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .routers import cart_router, wallet_router, user_router, order_router, supermarket_router
+from .routers import cart_router, wallet_router, user_router, order_router, supermarket_router, items_router
 
 master_router = APIRouter()
 master_router.include_router(cart_router)
@@ -7,5 +7,6 @@ master_router.include_router(wallet_router)
 master_router.include_router(user_router)
 master_router.include_router(order_router)
 master_router.include_router(supermarket_router)
+master_router.include_router(items_router)
 
 
