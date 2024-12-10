4dff98f336feaee3035ab9f2aae41db8a684c02c
Adham Sameh <adham.s.salama@gmail.com>
2024-12-10 20:23:41 +0400

    Fixed issue with items feed

--- server/api/routers/items/items.py
+++ server/api/routers/items/items.py
@@ -45,21 +45,13 @@ async def get_items_by_category_and_supermarket(
         category_name=category_name,
         items=[
             ItemResponse(
-                id=1,
-                name="Milk",
-                photo_url="https://cdn.theatlantic.com/thumbor/MAp_MfXXLZpH6CyMHf9LfaJjdnM=/458x43:1523x1108/540x540/media/img/mt/2024/10/Atlantic_Milk_2000x1125/original.jpg",
-                price=5.0,
-                description="Fresh milk from local farms.",
-                supermarket_id=supermarket_id
-            ),
-            ItemResponse(
-                id=2,
-                name="Cheese",
-                photo_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXhle2NDAmRQVL_u0WqGO7A6kxdisfpCxj-Q&s",
-                price=7.0,
-                description="Delicious cheddar cheese.",
-                supermarket_id=supermarket_id
-
+                id=item.id,
+                name=item.name,
+                photo_url=item.photo_url,
+                price=item.price,
+                description=item.description,
+                supermarket_id=item.supermarket_id
             )
+            for item in items
         ]
     )
